1fc312be151737d4321379b61984c78afd77d279
Ben Robinson <mentionbenjamin@gmail.com>
2018-06-18 17:41:35 +0100

    added index & new page

--- app.rb
+++ app.rb
@@ -3,7 +3,20 @@ require( 'sinatra/contrib/all' )
 require_relative('controllers/note_controller')
 require_relative('controllers/day_controller')
 require_relative('controllers/todo_controller')
+also_reload("./models/*")
 
-get '/' do
-  erb( :index )
+
+#INDEX
+get '/planner' do
+  erb(:index)
 end
+
+#NEW
+get "/planner/new" do
+  erb(:new)
+end
+
+# #SHOW
+# get "/planner/:id" do
+#   @
+# end
